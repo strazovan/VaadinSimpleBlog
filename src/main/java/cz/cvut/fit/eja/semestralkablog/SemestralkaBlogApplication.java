ed73955bd7edf4d1408b03991f98923ba5ee5ad4
strazdav <strazdav@fit.cvut.cz>
2017-06-09 18:21:04 +0200

    Initial commit

---
+++ src/main/java/cz/cvut/fit/eja/semestralkablog/SemestralkaBlogApplication.java
@@ -0,0 +1,12 @@
+package cz.cvut.fit.eja.semestralkablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralkaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralkaBlogApplication.class, args);
+	}
+}
---
+++ src/test/java/cz/cvut/fit/eja/semestralkablog/SemestralkaBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.cvut.fit.eja.semestralkablog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SemestralkaBlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
